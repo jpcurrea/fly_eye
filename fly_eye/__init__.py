ff42199fd05724d334061526115a709a613fd196
jpcurrea <johnpaulcurrea@gmail.com>
2019-07-14 15:05:26 -0400

    revert to using m +- 3 sd for saturations and values in colorSelector.

--- fly_eye/__init__.py
+++ fly_eye/__init__.py
@@ -1023,10 +1023,10 @@ class ColorSelector():
         self.hue_low = self.colors[:, 0].min()
         self.hue_hi = self.colors[:, 0].max()
         # hue_low, hue_hi = np.percentile(self.hsv[:, 0], [.5, 99.5])
-        self.sats_low, self.sats_hi = np.percentile(self.hsv[:, 1], [2.5, 97.5])
-        # sats_low, sats_hi = self.colors[:, 1].min(), self.colors[:, 1].max()
-        self.vals_low, self.vals_hi = np.percentile(self.hsv[:, 2], [2.5, 97.5])
-        # vals_low, vals_hi = self.colors[:, 2].min(), self.colors[:, 2].max()
+        # self.sats_low, self.sats_hi = np.percentile(self.hsv[:, 1], [2.5, 97.5])
+        sats_low, sats_hi = self.colors[:, 1].min(), self.colors[:, 1].max()
+        # self.vals_low, self.vals_hi = np.percentile(self.hsv[:, 2], [2.5, 97.5])
+        vals_low, vals_hi = self.colors[:, 2].min(), self.colors[:, 2].max()
         if self.hue_low < 0:    # if range overlaps 0, use or logic
             self.hue_low = 1 + self.hue_low
             comp_func = np.logical_or
@@ -1080,7 +1080,7 @@ class ColorSelector():
             h_std = stats.circstd(self.hsv[:, :, 0].flatten(), 0, 1)
             m = np.append(h_mean, m)
             sd = np.append(h_std, sd)
-            self.colors = np.array([m-2*sd, m+2*sd])
+            self.colors = np.array([m-3*sd, m+3*sd])
             self.keyed = self.select_color()
             self.img.set_array(self.keyed.astype('uint8'))
             self.hue_dist = list(np.histogram(
