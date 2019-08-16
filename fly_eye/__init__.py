6676ca5e3fe303c8b9b80eb5ded2fb2a20175552
jpcurrea <johnpaulcurrea@gmail.com>
2019-08-16 15:54:10 -0400

    fix typo

--- fly_eye/__init__.py
+++ fly_eye/__init__.py
@@ -457,10 +457,10 @@ class Eye(Layer):
         # optimum = np.squeeze(
         #     peak_local_max(fs*peaks, num_peaks=10, min_distance=10))  # second highest maximum
         optimum = np.squeeze(
-            peak_local_max(peaks, num_peaks=2, exclude_border=True))
+            peak_local_max(fs*peaks, num_peaks=2, exclude_border=True))
         optimum = optimum[optimum > np.sqrt(min_facets)]
         # optimum = optimum.min()
-        optimum = optimum[np.argmax(optimum)]
+        optimum = optimum[np.argmax(peaks[optimum])]
 
         # lower_bound = peak_local_max(peaks.max() - peaks[:optimum],
         #                              num_peaks=1)
